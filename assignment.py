a5c48dae04a77a57195fd15e5d5f6744bf4d2daf
Carol <carol.li1016@gmail.com>
2019-09-06 16:46:19 +0800

    Add assignment and nested file

---
+++ assignment.py
@@ -0,0 +1,10 @@
+# while True:
+#     name = input('Enter name:')
+#     if name == 'stop': break
+#     age = input('Enter age: ')
+#     print('Hello', name, '=>', int(age) ** 2)
+
+prod = 1
+for item in [1, 2, 3, 4]: prod *= item
+print(item)
+print(prod)
